b7f515d83600706cb538a6bd268bc6efa870ff7c
Dmitry <demonx.blenderist@gmail.com>
2019-05-05 17:36:13 +0300

    Добавлен класс ClasterManager

---
+++ app/ClasterManager.js
@@ -0,0 +1,65 @@
+const TelegramBot = require(ROOT_DIR+'/Components/BotManager/TelegramBot');
+const cluster = require('cluster');
+const BotManager = require(ROOT_DIR+'/Components/BotManager/BotManager');
+
+
+/**
+ * Класс отвечает за работу с кластером процессов
+ */
+class BotClusterManager {
+  /**
+   * конструктор менеджера кластера
+   */
+  constructor() {
+    this._oBot = {};
+  }
+
+  /**
+   * Производит запуск всех ботов
+   */
+  launchBots() {
+    BotManager.getBots()
+        .then((aBots)=>this.startForks(aBots));
+  }
+
+  /**
+   * запускает форк процессы. Передает им данные о ботах
+   * @param {array} aBots
+   */
+  startForks(aBots) {
+    aBots.forEach(this.startFork);
+  }
+
+  /**
+   * запускает форк процесс с ботом
+   * @param {object} oBot
+   */
+  startFork(oBot) {
+    const worker = cluster.fork();
+    worker.send(oBot);
+  }
+
+  /**
+   * @param {object} oBot
+   * запускает бота в форк процессе
+   * из процесса master передается конфиг бота
+   */
+  createTelegramBot(oBot) {
+    this._oBot = new TelegramBot(oBot);
+  }
+}
+
+const oBotCluster = new BotClusterManager();
+if (cluster.isMaster) {
+  oBotCluster.launchBots();
+  require(ROOT_DIR+'/web/init');
+} else {
+  process.on('message', (msg) => {
+    oBotCluster.createTelegramBot(msg);
+  });
+}
+
+
+module.exports = BotClusterManager;
+
+
