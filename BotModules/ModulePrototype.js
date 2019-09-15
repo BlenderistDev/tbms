07512adc79bc30c6ffbf4e28449fdf4a3e6ed42d
blenderist <demonx.blenderist@gmail.com>
2019-09-15 11:26:45 +0300

    add docker

--- BotModules/ModulePrototype.js
+++ BotModules/ModulePrototype.js
@@ -2,12 +2,7 @@
  * протопип модулей для ботов
  */
 class ModulePrototype {
-  /**
-   * @param {object} oConfig
-   */
-  constructor(oConfig) {
-    this.oConfig = oConfig;
-  }
+
 }
 
 module.exports = ModulePrototype;
